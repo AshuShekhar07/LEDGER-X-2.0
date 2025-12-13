511dc8ddc74da378fc4c7b2b2d94896f77923f91
Dharmesh <dharmeshgopnarayan29@gmail.com>
2025-12-13 23:27:02 +0530

    Change 2

--- database.py
+++ database.py
@@ -12,8 +12,12 @@ DATABASE_URL = os.getenv("DATABASE_URL")
 if not DATABASE_URL:
     raise ValueError("DATABASE_URL environment variable is not set")
 
+connect_args = {}
+if "sqlite" in DATABASE_URL:
+    connect_args["check_same_thread"] = False
+
 engine = create_engine(
-    DATABASE_URL, connect_args={"check_same_thread": False}
+    DATABASE_URL, connect_args=connect_args
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
